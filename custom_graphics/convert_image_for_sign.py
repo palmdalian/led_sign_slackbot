d301c5f71b427aa1208735d90995155bff9e1261
Michael Hand <palmdalian@users.noreply.github.com>
2017-09-23 22:55:55 -0700

    Clean things up a bit

--- custom_graphics/convert_image_for_sign.py
+++ custom_graphics/convert_image_for_sign.py
@@ -1,6 +1,9 @@
 import sys
 import cv2
 
+target_width = 18
+target_height = 7
+
 def conform_colors(img):
 	for line_index, line in enumerate(img):
 		for pixel_index, pixel in enumerate(line):
@@ -36,6 +39,7 @@ def conform_colors(img):
 					img[line_index][pixel_index] = [0, 0, 0]
 	return img
 
+
 def pad_matrix(target_width, target_height, sign_matrix, fill_color):
 	if len(sign_matrix) < target_height:
 		pad = target_height - len(sign_matrix)
@@ -68,64 +72,71 @@ def pad_matrix(target_width, target_height, sign_matrix, fill_color):
 	return sign_matrix
 
 
-#IN BGR instead of RGB
-image = cv2.imread (sys.argv[1], cv2.IMREAD_UNCHANGED)
-height, width, channels = image.shape
-target_width = 18
-target_height = 7
+def load_and_resize(path):
+	#IN BGR instead of RGB
+	image = cv2.imread (path, cv2.IMREAD_UNCHANGED)
+	height, width, channels = image.shape
 
-if float(width / height) <= float(target_width / target_height):
-	# Height is the restriction
-	ratio = float(target_height) / float(height)
-	print(ratio)
-	image = cv2.resize(image, (0, 0), fx=ratio, fy=ratio)
-else:
-	# Width is the restriction
-	ratio = float(target_width) / float(width)
-	print(ratio)
-	image = cv2.resize(image, (0, 0), fx=ratio, fy=ratio)
+	if float(width / height) <= float(target_width / target_height):
+		# Height is the restriction
+		ratio = float(target_height) / float(height)
+		print(ratio)
+		image = cv2.resize(image, (0, 0), fx=ratio, fy=ratio)
+	else:
+		# Width is the restriction
+		ratio = float(target_width) / float(width)
+		print(ratio)
+		image = cv2.resize(image, (0, 0), fx=ratio, fy=ratio)
 
-height, width, channels = image.shape
-print("NEW height {} width {}".format(height, width))
+	height, width, channels = image.shape
+	print("NEW height {} width {}".format(height, width))
+	return image, ratio
 
-xpad = 0
-ypad = 0
-sign_matrix = []
 
-for line in image:
-	sign_matrix.append([])
-	for pixel in line:
-		B = pixel[0]
-		G = pixel[1]
-		R = pixel[2]
-		if len(pixel) > 3:
-			A = pixel[3]
-		else:
-			A = 255
-		if A < 100:
-			sign_matrix[-1].append("B")
-		if (R > 200 and G > 200 and B > 200):
-			# Turn white to yellow for now
-			sign_matrix[-1].append("Y")
-		elif R > 200 and G > 100:
-			sign_matrix[-1].append("Y")
-		elif G > 150:
-			sign_matrix[-1].append("G")
-			B = 0
-		elif R > 150:
-			sign_matrix[-1].append("R")
-		else:
-			sign_matrix[-1].append("B")
+def build_matrix(image):
+	sign_matrix = []
+	for line in image:
+		sign_matrix.append([])
+		for pixel in line:
+			B = pixel[0]
+			G = pixel[1]
+			R = pixel[2]
+			if len(pixel) > 3:
+				A = pixel[3]
+			else:
+				A = 255
+			if A < 100:
+				sign_matrix[-1].append("B")
+			elif (R > 200 and G > 200 and B > 200):
+				# Turn white to yellow for now
+				sign_matrix[-1].append("Y")
+			elif R > 200 and G > 100:
+				sign_matrix[-1].append("Y")
+			elif G > 150:
+				sign_matrix[-1].append("G")
+				B = 0
+			elif R > 150:
+				sign_matrix[-1].append("R")
+			else:
+				sign_matrix[-1].append("B")
+	return sign_matrix
 
-final_matrix = pad_matrix(target_width, target_height, sign_matrix, "B")
-image_string = ""
-for row in final_matrix:
-	image_string += "".join(row)
 
-print image_string
+def get_image_string(matrix):
+	image_string = ""
+	for row in matrix:
+		image_string += "".join(row)
 
-image = conform_colors(image)
-big = cv2.resize(image, (0, 0), fx=1/ratio, fy=1/ratio, interpolation = cv2.INTER_NEAREST)
-cv2.imshow('image',big)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+	image, resize_ratio = load_and_resize(sys.argv[1])
+	sign_matrix = build_matrix(image)
+	final_matrix = pad_matrix(target_width, target_height, sign_matrix, "B")
+	image_string = get_image_string(final_matrix)
+
+	print image_string
+
+	image = conform_colors(image)
+	big = cv2.resize(image, (0, 0), fx=1/resize_ratio, fy=1/resize_ratio, interpolation = cv2.INTER_NEAREST)
+	cv2.imshow('image',big)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
